bac8504570deb10eb194603ef2c589accb3ff6e6
kobir079 <it24030@mbstu.ac.bd>
2026-01-28 15:14:08 +0600

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -1,1 +1,55 @@
+abstract class Shape {
+    abstract double area();
+}
+
+interface Drawable {
+    void draw();
+}
+
+class Circle extends Shape implements Drawable {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    double area() {
+        return 3.1416 * radius * radius;
+    }
+
+    public void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Rectangle extends Shape implements Drawable {
+    double length, width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    double area() {
+        return length * width;
+    }
+
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape s1 = new Circle(5);
+        Shape s2 = new Rectangle(4, 6);
+        Drawable d1 = new Circle(3);
+        Drawable d2 = new Rectangle(2, 8);
+
+        System.out.println(s1.area());
+        System.out.println(s2.area());
+        d1.draw();
+        d2.draw();
+    }
+}
 
